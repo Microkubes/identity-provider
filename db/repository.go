754ab264384557a5801b8c93cd3f094b712ef3c6
Nina Zid <zidrovska.nina@gmail.com>
2018-08-08 13:53:40 +0200

    Removed mongo implemented functions

--- db/repository.go
+++ db/repository.go
@@ -31,57 +31,57 @@ type Repository interface {
 }
 
 // MongoCollection wraps a mgo.Collection to embed methods in models.
-type MongoCollections struct {
-	Services *mgo.Collection
-	Sessions *mgo.Collection
-}
+// type MongoCollections struct {
+// 	Services *mgo.Collection
+// 	Sessions *mgo.Collection
+// }
 
 // NewSession returns a new Mongo Session.
-func NewSession(Host string, Username string, Password string, Database string) *mgo.Session {
-	session, err := mgo.DialWithInfo(&mgo.DialInfo{
-		Addrs:    []string{Host},
-		Username: Username,
-		Password: Password,
-		Database: Database,
-		Timeout:  30 * time.Second,
-	})
-	if err != nil {
-		panic(err)
-	}
+// func NewSession(Host string, Username string, Password string, Database string) *mgo.Session {
+// 	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+// 		Addrs:    []string{Host},
+// 		Username: Username,
+// 		Password: Password,
+// 		Database: Database,
+// 		Timeout:  30 * time.Second,
+// 	})
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	// SetMode - consistency mode for the session.
-	session.SetMode(mgo.Monotonic, true)
+// 	// SetMode - consistency mode for the session.
+// 	session.SetMode(mgo.Monotonic, true)
 
-	return session
-}
+// 	return session
+// }
 
 // PrepareDB ensure presence of persistent and immutable data in the DB.
-func PrepareDB(session *mgo.Session, db string, dbCollection string, indexes []string) *mgo.Collection {
-	// Create collection
-	collection := session.DB(db).C(dbCollection)
+// func PrepareDB(session *mgo.Session, db string, dbCollection string, indexes []string) *mgo.Collection {
+// 	// Create collection
+// 	collection := session.DB(db).C(dbCollection)
 
-	// Define indexes
-	for _, elem := range indexes {
-		i := []string{elem}
-		index := mgo.Index{}
-		if dbCollection == "sessions" {
-			index.Key = i
-			index.Unique = true
-			index.Background = true
-			index.Sparse = true
-			index.ExpireAfter = time.Duration(86400) * time.Second
-		} else {
-			index.Key = i
-			index.Unique = true
-			index.Background = true
-			index.Sparse = true
-		}
+// 	// Define indexes
+// 	for _, elem := range indexes {
+// 		i := []string{elem}
+// 		index := mgo.Index{}
+// 		if dbCollection == "sessions" {
+// 			index.Key = i
+// 			index.Unique = true
+// 			index.Background = true
+// 			index.Sparse = true
+// 			index.ExpireAfter = time.Duration(86400) * time.Second
+// 		} else {
+// 			index.Key = i
+// 			index.Unique = true
+// 			index.Background = true
+// 			index.Sparse = true
+// 		}
 
-		// Create indexes
-		if err := collection.EnsureIndex(index); err != nil {
-			panic(err)
-		}
-	}
+// 		// Create indexes
+// 		if err := collection.EnsureIndex(index); err != nil {
+// 			panic(err)
+// 		}
+// 	}
 
-	return collection
-}
+// 	return collection
+// }
--- db/service.go
+++ db/service.go
@@ -15,58 +15,58 @@ import (
 // which is typically the service provider's metadata URL. If an appropriate service
 // provider cannot be found then the returned error must be os.ErrNotExist.
 func (m *MongoCollections) GetServiceProvider(r *http.Request, serviceProviderID string) (*saml.EntityDescriptor, error) {
-	service := &samlidp.Service{}
-	query := bson.M{"name": bson.M{"$eq": serviceProviderID}}
-	err := m.Services.Find(query).Limit(1).One(service)
-	if err != nil {
-		if err.Error() == "not found" {
-			return nil, os.ErrNotExist
-		} else {
-			return nil, goa.ErrInternal(err)
-		}
-	}
+	// service := &samlidp.Service{}
+	// query := bson.M{"name": bson.M{"$eq": serviceProviderID}}
+	// err := m.Services.Find(query).Limit(1).One(service)
+	// if err != nil {
+	// 	if err.Error() == "not found" {
+	// 		return nil, os.ErrNotExist
+	// 	} else {
+	// 		return nil, goa.ErrInternal(err)
+	// 	}
+	// }
 
-	return &service.Metadata, nil
+	// return &service.Metadata, nil
 }
 
 // AddServiceProvider register new service provider
 func (m *MongoCollections) AddServiceProvider(service *samlidp.Service) error {
-	_, err := m.Services.Upsert(
-		bson.M{"name": service.Name},
-		bson.M{"$set": service})
+	// _, err := m.Services.Upsert(
+	// 	bson.M{"name": service.Name},
+	// 	bson.M{"$set": service})
 
-	if err != nil {
-		return goa.ErrInternal(err)
-	}
+	// if err != nil {
+	// 	return goa.ErrInternal(err)
+	// }
 
-	return nil
+	// return nil
 }
 
 // DeleteServiceProvider deletes the service by serviceID which is EntityID
 func (m *MongoCollections) DeleteServiceProvider(serviceID string) error {
-	selector := bson.M{"name": bson.M{"$eq": serviceID}}
-	err := m.Services.Remove(selector)
-	if err != nil {
-		if err.Error() == "not found" {
-			return goa.ErrNotFound("service not found")
-		} else {
-			return goa.ErrInternal(err)
-		}
-	}
+	// selector := bson.M{"name": bson.M{"$eq": serviceID}}
+	// err := m.Services.Remove(selector)
+	// if err != nil {
+	// 	if err.Error() == "not found" {
+	// 		return goa.ErrNotFound("service not found")
+	// 	} else {
+	// 		return goa.ErrInternal(err)
+	// 	}
+	// }
 
-	return nil
+	// return nil
 }
 
 // GetServiceProviders returns all SP
 func (m *MongoCollections) GetServiceProviders() (*[]samlidp.Service, error) {
-	var services []samlidp.Service
-	if err := m.Services.Find(nil).All(&services); err != nil {
-		return nil, goa.ErrInternal(err)
-	}
+	// var services []samlidp.Service
+	// if err := m.Services.Find(nil).All(&services); err != nil {
+	// 	return nil, goa.ErrInternal(err)
+	// }
 
-	if len(services) == 0 {
-		return nil, goa.ErrNotFound("no services found!")
-	}
+	// if len(services) == 0 {
+	// 	return nil, goa.ErrNotFound("no services found!")
+	// }
 
-	return &services, nil
+	// return &services, nil
 }
